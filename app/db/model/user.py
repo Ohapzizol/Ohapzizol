c2da505673d2befd8f2d8a9139f8b29d68ce6366
h4n-ul <jun061119@gmail.com>
2024-07-16 12:04:33 +0900

    FEAT :: PAY와 USER 테이블 생성

---
+++ app/db/model/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, VARCHAR, BIGINT
+from ..base import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    name = Column(VARCHAR(7), primary_key=True, nullable=False)
+    id = Column(VARCHAR(15), primary_key=True, nullable=False)
+    password = Column(VARCHAR(60), nullable=False)
+    balance = Column(BIGINT, default=0)
