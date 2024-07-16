648c277f2408401b939f03d7537dfaaff150078e
h4n-ul <jun061119@gmail.com>
2024-07-16 12:51:34 +0900

    REFACT :: Added pk and fk for pay via userid

--- app/db/models/pay.py
+++ app/db/models/pay.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Column, VARCHAR, DATETIME, BIGINT
-from app.db.base import Base
+from sqlalchemy import Column, VARCHAR, DATETIME, BIGINT, ForeignKey
+from sqlalchemy.orm import relationship
+from ..base import Base
 
 class Pay(Base):
     __tablename__ = "pay"
 
-    name = Column(VARCHAR(30), primary_key=True, nullable=False)
+    id = Column(BIGINT, primary_key=True, autoincrement=True)
+    name = Column(VARCHAR(30), nullable=False)
     value = Column(BIGINT, nullable=False)
     description = Column(VARCHAR(500))
     date = Column(DATETIME, nullable=False)
     tag = Column(VARCHAR(10), nullable=False)
+    user_id = Column(VARCHAR(15), ForeignKey('user.id'), nullable=False)
+
+    user = relationship("User", back_populates="pays")
--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, VARCHAR, BIGINT
-
-from ..base_class import Base
-
+from sqlalchemy.orm import relationship
+from ..base import Base
 
 class User(Base):
     __tablename__ = "user"
+
+    name = Column(VARCHAR(7), primary_key=True, nullable=False)
     id = Column(VARCHAR(15), primary_key=True, nullable=False)
-    name = Column(VARCHAR(4), nullable=False)
-    password = Column(VARCHAR(15), nullable=False)
-    balance = Column(BIGINT, nullable=False)
+    password = Column(VARCHAR(60), nullable=False)
+    balance = Column(BIGINT, default=0)
+
+    pays = relationship("Pay", back_populates="user")
